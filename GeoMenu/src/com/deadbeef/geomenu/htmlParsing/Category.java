358404d2589f836f21f52d0168bd430011ccdbdf
Cahlen Brancheau <nokeo08@gmail.com>
2014-04-05 15:54:26 -0500

    CB: Added isEaten and isFavorite to Category.java

--- GeoMenu/src/com/deadbeef/geomenu/htmlParsing/Category.java
+++ GeoMenu/src/com/deadbeef/geomenu/htmlParsing/Category.java
@@ -1,12 +1,29 @@
 package com.deadbeef.geomenu.htmlParsing;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+
+
+
+
+
+
+import java.util.Locale;
+
+import android.content.Context;
+import android.util.Log;
 
 public class Category {
 	
 	private ArrayList<Entree> entrees;
 	private String categoryName;
 	private String restaurantName;
+	private final String IFEATENLFILE = "IFEATENLFILE";
+	private final String ISFAVORITELISTFILE = "ISFAVORITELISTFILE";
+	
+	private Context fileContext;
 	
 	protected Category(String categoryName, String restaurantName) {
 		this.categoryName = categoryName;
@@ -27,5 +44,45 @@ public class Category {
 	
 	public ArrayList<Entree> getEntrees() {
 		return entrees;
+	}
+	
+	private boolean isEaten(String restaurantName, String name)
+	{	
+		String itemName = restaurantName+=name;
+		String str= "";
+		
+		try {
+			FileInputStream fis = fileContext.openFileInput(IFEATENLFILE);
+			byte[] buffer = new byte[(int) fis.getChannel().size()];
+	        fis.read(buffer);
+	        for(byte b:buffer) str+=(char)b;
+	        fis.close();
+	        Log.i("STACKOVERFLOW", String.format("GOT: [%s]", str));
+	    } catch (IOException e) {
+	        Log.e("STACKOVERFLOW", e.getMessage(), e);
+	    }
+		
+		
+		return str.toLowerCase(Locale.getDefault()).contains(itemName.toLowerCase(Locale.getDefault()));
+	}
+	
+	private boolean isFavorite(String restaurantName, String name)
+	{
+		String itemName = restaurantName+=name;
+		String str= "";
+		
+		try {
+			FileInputStream fis = fileContext.openFileInput(ISFAVORITELISTFILE);
+			byte[] buffer = new byte[(int) fis.getChannel().size()];
+	        fis.read(buffer);
+	        for(byte b:buffer) str+=(char)b;
+	        fis.close();
+	        Log.i("STACKOVERFLOW", String.format("GOT: [%s]", str));
+	    } catch (IOException e) {
+	        Log.e("STACKOVERFLOW", e.getMessage(), e);
+	    }
+		
+		
+		return str.toLowerCase(Locale.getDefault()).contains(itemName.toLowerCase(Locale.getDefault()));
 	}
 }

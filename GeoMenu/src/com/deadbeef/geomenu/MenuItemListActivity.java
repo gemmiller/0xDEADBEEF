6d313cc20c4d4a449534810a562da8bc653b1096
gemiller100 <geoffrey.e.miller.isu@gmail.com>
2014-04-05 10:30:11 -0500

    Added Location Updating
    App now shows a toast when location changes. the current location is now
    updated.

--- GeoMenu/src/com/deadbeef/geomenu/MenuItemListActivity.java
+++ GeoMenu/src/com/deadbeef/geomenu/MenuItemListActivity.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
+import com.google.android.gms.location.LocationListener;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -15,6 +16,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 
 
 /**
@@ -35,7 +37,8 @@ import com.google.android.gms.location.LocationClient;
  */
 public class MenuItemListActivity extends FragmentActivity
         implements MenuItemListFragment.Callbacks, GooglePlayServicesClient.ConnectionCallbacks,
-        GooglePlayServicesClient.OnConnectionFailedListener {
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
@@ -44,7 +47,19 @@ public class MenuItemListActivity extends FragmentActivity
     private boolean mTwoPane;
     private Location loc;
     private LocationClient loc_client;
-
+ // Milliseconds per second
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    // Update frequency in seconds
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    // The fastest update frequency, in seconds
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+    LocationRequest mLocationRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +79,14 @@ public class MenuItemListActivity extends FragmentActivity
                     .setActivateOnItemClick(true);
         }
         loc_client = new LocationClient(this,this,this);
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
         // TODO: If exposing deep links into your app, handle intents here.
     }
@@ -206,8 +229,8 @@ public class MenuItemListActivity extends FragmentActivity
         // Display the connection status
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
         loc = loc_client.getLastLocation();
-        Toast.makeText(this, loc.getLatitude()+ "," +loc.getLongitude(),Toast.LENGTH_LONG).show();
-
+        //Toast.makeText(this, loc.getLatitude()+ "," +loc.getLongitude(),Toast.LENGTH_LONG).show();
+        loc_client.requestLocationUpdates(mLocationRequest,this);
     }
     
     /*
@@ -255,6 +278,19 @@ public class MenuItemListActivity extends FragmentActivity
            // showErrorDialog(connectionResult.getErrorCode());
         }
     }
+    
+ // Define the callback method that receives location updates
+    @Override
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+    	loc = location;
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
  
 }
 
